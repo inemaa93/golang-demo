0f8a0e3e40af1cec29ffe4b78416969049ca1bd0
Ine <inaaa6@gmail.com>
2026-01-07 13:21:46 +0100

    Added Values

--- main.go
+++ main.go
@@ -16,6 +16,14 @@ func main() {
 	// Hello World
 	fmt.Println("hello world")
 
+	// Values
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
 	// hash password
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
