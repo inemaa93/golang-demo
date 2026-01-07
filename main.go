b6bd5d1c4668a9ae273f9b908687d93c9772db8a
Ine <inaaa6@gmail.com>
2026-01-07 13:35:02 +0100

    Added Variables

--- main.go
+++ main.go
@@ -24,6 +24,18 @@ func main() {
 	fmt.Println(true || false)
 	fmt.Println(!true)
 
+	// Variables
+	var a = "initial"
+	fmt.Println(a)
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+	var d = true
+	fmt.Println(d)
+	var e int
+	fmt.Println(e)
+	f := "apple"
+	fmt.Print(f)
+	
 	// hash password
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
